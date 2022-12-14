e16ee2c35e7c8e58b8915dd5857e99f435a8cd63
ngiamdw <njj64@hotmail.com>
2022-12-14 11:44:13 +0800

    overtimeApp , LeaveSchema annotations 1st commit

--- src/main/java/sg/nus/iss/leavesystem/ca/model/LeaveScheme.java
+++ src/main/java/sg/nus/iss/leavesystem/ca/model/LeaveScheme.java
@@ -1,10 +1,38 @@
 package sg.nus.iss.leavesystem.ca.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class LeaveScheme {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
 	
-	public String id;
+	
+	
 	public String employmentScheme;
+	
 	public double annualLeaveEntitlement;
 	public double medicalLeaveEntitlement;
+	
+	
+	@OneToOne(mappedBy = "leaveScheme")
+	public Staff staff;
+	
+	public LeaveScheme(String employmentScheme, double annualLeaveEntitlement, double medicalLeaveEntitlement) {
+		super();
+		this.employmentScheme = employmentScheme;
+		this.annualLeaveEntitlement = annualLeaveEntitlement;
+		this.medicalLeaveEntitlement = medicalLeaveEntitlement;
+	}
 
+	
 }
--- src/main/java/sg/nus/iss/leavesystem/ca/model/OvertimeApplication.java
+++ src/main/java/sg/nus/iss/leavesystem/ca/model/OvertimeApplication.java
@@ -3,17 +3,44 @@ package sg.nus.iss.leavesystem.ca.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class OvertimeApplication {
 	
-	public String id; //
-	public Staff employee;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id; //
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "staff_id")
+	public Staff staff;
+	
+	
 	public LocalDateTime startDateTime; //
 	public LocalDateTime endDateTime; //
+	
 	public String employeeComment; //
+	
 	public LocalDateTime appliedDateTime; //
+	
+	
 	public String approvalStatus; //
 	
 	public String managerRemarks;//
 	public LocalDate dateApplicationReviewed; //
 	
+	
+	
 }
+
