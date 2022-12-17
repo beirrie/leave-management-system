cf8a596be4bf4deb4ff01ad1d915365dc0880974
beirrie <64721606+beirrie@users.noreply.github.com>
2022-12-17 22:27:52 +0800

    add validation for roles

--- src/main/java/sg/nus/iss/leavesystem/ca/validator/UserStaffFormValidator.java
+++ src/main/java/sg/nus/iss/leavesystem/ca/validator/UserStaffFormValidator.java
@@ -1,16 +1,23 @@
 package sg.nus.iss.leavesystem.ca.validator;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import sg.nus.iss.leavesystem.ca.model.LeaveScheme;
+import sg.nus.iss.leavesystem.ca.model.Staff;
 import sg.nus.iss.leavesystem.ca.model.dto.UserStaffForm;
 import sg.nus.iss.leavesystem.ca.service.LeaveSchemeService;
+import sg.nus.iss.leavesystem.ca.service.StaffService;
 
 @Component
 public class UserStaffFormValidator implements Validator {
+
+	@Autowired
+	private StaffService staffService;
 
 	@Autowired
 	private LeaveSchemeService leaveSchemeService;
@@ -37,6 +44,22 @@ public class UserStaffFormValidator implements Validator {
 			errors.rejectValue("annualLeaveBalance", "error.annualLeaveBalance",
 					"Number cannot be more than " + leaveScheme.getAnnualLeaveEntitlement());
 		}
+		List<String> roles = userStaffForm.getRoles().stream().map(r -> r.getRoleName().toLowerCase()).toList();
+		boolean isManager = roles.contains("manager");
+		boolean isEmployee = roles.contains("employee");
+
+		if (!isManager) {
+			Staff staff = staffService.findStaffByID(userStaffForm.getStaffId());
+			if (staff.getSubordinates().size() != 0) {
+				errors.rejectValue("roles", "error.roles",
+						"Manager with subordinates cannot be set to an employee role.");
+			}
+		}
+
+		if (isManager && isEmployee) {
+			errors.rejectValue("roles", "error.roles", "Employee and Manager cannot be selected together.");
+		}
+
 	}
 
 }
