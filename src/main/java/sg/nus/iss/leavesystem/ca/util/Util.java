de1f272005c9bd4cd39da592f3e0add8892b4067
Shaun_Lin <shanfu87@yahoo.com>
2022-12-19 18:41:02 +0800

    added utility files of mabel

--- src/main/java/sg/nus/iss/leavesystem/ca/util/Util.java
+++ src/main/java/sg/nus/iss/leavesystem/ca/util/Util.java
@@ -1,10 +1,27 @@
 package sg.nus.iss.leavesystem.ca.util;
 
+<<<<<<< HEAD
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+
+    static List<LocalDateTime> holidays = new ArrayList<>();
+
+    public Util() {
+
+    }
+
+=======
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Util {
 
+>>>>>>> Manager
     public static String convertDateToString(LocalDateTime localDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return localDate.format(formatter);
@@ -16,4 +33,42 @@ public class Util {
         String dateStr = args[2] + "-" + args[1] + "-" + args[0] + " 00:00";
         return LocalDateTime.parse(dateStr, formatter);
     }
+<<<<<<< HEAD
+
+    public static Boolean isWeekend(LocalDateTime date) {
+        if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY)
+            return true;
+        else
+            return false;
+    }
+
+    public static List<LocalDateTime> prepareHoliday() {
+        holidays = new ArrayList<>();
+        holidays.add(convertStringToDate("26/12/2022"));
+        holidays.add(convertStringToDate("02/01/2023"));
+        holidays.add(convertStringToDate("23/01/2023"));
+        holidays.add(convertStringToDate("24/01/2023"));
+        holidays.add(convertStringToDate("07/04/2023"));
+        holidays.add(convertStringToDate("01/05/2023"));
+        holidays.add(convertStringToDate("02/06/2023"));
+        holidays.add(convertStringToDate("29/06/2023"));
+        holidays.add(convertStringToDate("09/08/2023"));
+        holidays.add(convertStringToDate("13/11/2023"));
+        holidays.add(convertStringToDate("25/12/2023"));
+        return holidays;
+    }
+
+    public static Boolean isPublicHoliday(LocalDateTime date) {
+        if (holidays.size() == 0)
+            holidays = prepareHoliday();
+
+        for (LocalDateTime localDateTime : holidays) {
+            if (localDateTime.isEqual(date))
+                return true;
+        }
+        return false;
+    }
 }
+=======
+}
+>>>>>>> Manager
